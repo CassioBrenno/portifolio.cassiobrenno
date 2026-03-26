40ebc760792453798e5030ebe752c97522e6e86f
Cássio Brenno <b.br3nn0@gmail.com>
2026-03-25 22:11:42 -0300

    Update Ellipse.tsx

--- src/components/Hero/Ellipse.tsx
+++ src/components/Hero/Ellipse.tsx
@@ -1,65 +1,77 @@
-import { FC, HTMLAttributes, RefObject } from 'react'
+'use client'
 
-interface EllipseProps extends HTMLAttributes<SVGElement> {
-  ref: RefObject<SVGSVGElement>
+import { forwardRef } from 'react'
+
+interface EllipseProps extends React.SVGProps<SVGSVGElement> {
+ 
 }
 
-const Ellipse: FC<EllipseProps> = ({ ref, ...props }) => {
-  return (
-    <svg
-      ref={ref}
-      width="412"
-      height="413"
-      viewBox="0 0 412 413"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-      className="absolute left-0 top-0 size-56 transition-transform duration-500 ease-out sm:size-60 md:size-[20rem] lg:size-[25.75rem]"
-    >
-      <defs>
-        <linearGradient id="ellipseGradient" x1="0%" y1="0%" x2="100%" y2="100%" gradientUnits="objectBoundingBox">
+const Ellipse = forwardRef<SVGSVGElement, EllipseProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <svg
+        ref={ref}
+        width="412"
+        height="413"
+        viewBox="0 0 412 413"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+        {...props}
+      >
+        <defs>
+          <linearGradient 
+            id="ellipseGradient" 
+            x1="0%" 
+            y1="0%" 
+            x2="100%" 
+            y2="100%" 
+            gradientUnits="objectBoundingBox"
+          >
+            <stop offset="0%" stopColor="var(--p)" />
+            <stop offset="4%" stopColor="var(--p)" />
+            <stop offset="6%" stopColor="var(--s)" />
+            <stop offset="9%" stopColor="var(--a)" />
+            <stop offset="12%" stopColor="var(--n)" />
+            <stop offset="16%" stopColor="var(--p)" />
 
-          <stop offset="0%" stopColor="var(--p)" />
-          <stop offset="4%" stopColor="var(--p)" />
-          <stop offset="6%" stopColor="var(--s)" />
-          <stop offset="9%" stopColor="var(--a)" />
-          <stop offset="12%" stopColor="var(--n)" />
-          <stop offset="16%" stopColor="var(--p)" />
+            <stop offset="25%" stopColor="var(--p)" />
+            <stop offset="29%" stopColor="var(--s)" />
+            <stop offset="32%" stopColor="var(--a)" />
+            <stop offset="35%" stopColor="var(--n)" />
+            <stop offset="39%" stopColor="var(--p)" />
 
-          <stop offset="25%" stopColor="var(--p)" />
-          <stop offset="29%" stopColor="var(--s)" />
-          <stop offset="32%" stopColor="var(--a)" />
-          <stop offset="35%" stopColor="var(--n)" />
-          <stop offset="39%" stopColor="var(--p)" />
+            <stop offset="48%" stopColor="var(--p)" />
+            <stop offset="52%" stopColor="var(--s)" />
+            <stop offset="55%" stopColor="var(--a)" />
+            <stop offset="58%" stopColor="var(--n)" />
+            <stop offset="62%" stopColor="var(--p)" />
 
-          <stop offset="48%" stopColor="var(--p)" />
-          <stop offset="52%" stopColor="var(--s)" />
-          <stop offset="55%" stopColor="var(--a)" />
-          <stop offset="58%" stopColor="var(--n)" />
-          <stop offset="62%" stopColor="var(--p)" />
+            <stop offset="71%" stopColor="var(--p)" />
+            <stop offset="75%" stopColor="var(--s)" />
+            <stop offset="78%" stopColor="var(--a)" />
+            <stop offset="81%" stopColor="var(--n)" />
+            <stop offset="85%" stopColor="var(--p)" />
 
-          <stop offset="71%" stopColor="var(--p)" />
-          <stop offset="75%" stopColor="var(--s)" />
-          <stop offset="78%" stopColor="var(--a)" />
-          <stop offset="81%" stopColor="var(--n)" />
-          <stop offset="85%" stopColor="var(--p)" />
+            <stop offset="94%" stopColor="var(--p)" />
+            <stop offset="100%" stopColor="var(--p)" />
+          </linearGradient>
+        </defs>
 
-          <stop offset="94%" stopColor="var(--p)" />
-          <stop offset="100%" stopColor="var(--p)" />
-        </linearGradient>
-      </defs>
+        <circle
+          cx="206"
+          cy="206.401"
+          r="204.5"
+          stroke="url(#ellipseGradient)"
+          strokeWidth="6"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    )
+  }
+)
 
-      <circle
-        cx="206"
-        cy="206.401"
-        r="204.5"
-        stroke="url(#ellipseGradient)"
-        strokeWidth="6"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
+Ellipse.displayName = 'Ellipse'
 
 export default Ellipse
